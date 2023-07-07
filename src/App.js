aa86634509bf8edf138a59ead5d743bf32fca88f
Aryaman <aryamanraj123456789@gmail.com>
2023-07-07 20:15:05 +0530

    fixed_data_fetching

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import './App.css';
-import * as React from 'react';
-import Switch from '@mui/material/Switch';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { styled, useTheme } from '@mui/material/styles';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';  
-import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
-import { useEffect,useState } from 'react';
-import axios from 'axios';
+  import './App.css';
+  import * as React from 'react';
+  import Switch from '@mui/material/Switch';
+  import Box from '@mui/material/Box';
+  import Typography from '@mui/material/Typography';
+  import { styled, useTheme } from '@mui/material/styles';
+  import Drawer from '@mui/material/Drawer';
+  import CssBaseline from '@mui/material/CssBaseline';
+  import MuiAppBar from '@mui/material/AppBar';
+  import Toolbar from '@mui/material/Toolbar';
+  import List from '@mui/material/List';
+  import Divider from '@mui/material/Divider';
+  import IconButton from '@mui/material/IconButton';
+  import MenuIcon from '@mui/icons-material/Menu';
+  import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+  import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+  import ListItem from '@mui/material/ListItem';
+  import ListItemButton from '@mui/material/ListItemButton';
+  import ListItemIcon from '@mui/material/ListItemIcon';
+  import ListItemText from '@mui/material/ListItemText';  
+  import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+  import { useEffect,useState } from 'react';
+  import axios from 'axios';
+  import Aos from 'aos'
+  import 'aos/dist/aos.css'
+  import Card from '@mui/material/Card';
+  import CardActions from '@mui/material/CardActions';
+  import CardContent from '@mui/material/CardContent';
+  import Button from '@mui/material/Button';
 
 
 
@@ -30,253 +35,299 @@ import axios from 'axios';
 
 
 
-//------------------------Sidebar----------------------------------------------------------
+  //------------------------Sidebar----------------------------------------------------------
 
-const drawerWidth = 240;
+  const drawerWidth = 240;
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
     transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
     }),
-  }),
-}));
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
 
 
-function makeButton(data) {
-  return (
-      <ListItem key={data.id} disablePadding>
-      <ListItemButton >
-        <ListItemIcon>
-          <TipsAndUpdatesIcon /> 
-        </ListItemIcon>
-        <ListItemText primary={data.title} />
-      </ListItemButton>
-    </ListItem>
-  );
-}
+  // function makeButton({data,chosenData}) {
 
 
-function PersistentDrawerLeft({title}) {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  //   return (
+  //       <ListItem key={data.id} disablePadding>
+  //       <ListItemButton onClick={() => chosen(data)} >
+  //         <ListItemIcon>
+  //           <TipsAndUpdatesIcon /> 
+  //         </ListItemIcon>
+  //         <ListItemText primary={data.title} />
+  //       </ListItemButton>
+  //     </ListItem>
+  //   );
+  // }
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{background: 'transparent',boxShadow: 'none',zIndex:0}} open={open}>
-        <Toolbar className="bar" >
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon sx={{color:"black"}} />
-          </IconButton>
+  function PersistentDrawerLeft({title,setChosenData}) {
 
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+    const chosen = (data) =>{
+      setChosenData(data)
+    }
+
+
+    const makeButton = (data) =>{
+      return(
+        <ListItem key={data.id} disablePadding>
+        <ListItemButton onClick={() => chosen(data)} >
+          <ListItemIcon>
+            <TipsAndUpdatesIcon /> 
+          </ListItemIcon>
+          <ListItemText primary={data.title} />
+        </ListItemButton>
+      </ListItem>
+      )
+    }
+
+
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
+
+    return (
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" sx={{background: 'transparent',boxShadow: 'none',zIndex:0}} open={open}>
+          <Toolbar className="bar" >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            >
+              <MenuIcon sx={{color:"black"}} />
+            </IconButton>
+
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-        <Typography variant="h6" noWrap component="div">
-          Content from API
-          </Typography>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {title.map(makeButton,this)}
-        </List>
-        <Divider />
-      </Drawer>
-      </Box>
-  );
-  }
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+          <Typography variant="h6" noWrap component="div">
+            Content from API
+            </Typography>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {/* {title.map((makeButton,this))} */}
+            {title.map((title) => makeButton(title))}
+          </List>
+          <Divider />
+        </Drawer>
+        </Box>
+    );
+    }
 
 
-//----------------------------- Dark Mode Toggle Switch----------------------------------------------
+  //----------------------------- Dark Mode Toggle Switch----------------------------------------------
 
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  '& .MuiSwitch-switchBase': {
-    margin: 1,
-    padding: 0,
-    transform: 'translateX(6px)',
-    '&.Mui-checked': {
-      color: '#fff',
-      transform: 'translateX(22px)',
-      '& .MuiSwitch-thumb:before': {
+  const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+      margin: 1,
+      padding: 0,
+      transform: 'translateX(6px)',
+      '&.Mui-checked': {
+        color: '#fff',
+        transform: 'translateX(22px)',
+        '& .MuiSwitch-thumb:before': {
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+            '#fff',
+          )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+        },
+        '& + .MuiSwitch-track': {
+          opacity: 1,
+          backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#66a3ff',
+        },
+      },
+    },
+    '& .MuiSwitch-thumb': {
+      backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+      width: 32,
+      height: 32,
+      '&:before': {
+        content: "''",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
           '#fff',
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-      },
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#66a3ff',
+        )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
       },
     },
-  },
-  '& .MuiSwitch-thumb': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-    width: 32,
-    height: 32,
-    '&:before': {
-      content: "''",
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      left: 0,
-      top: 0,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        '#fff',
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    '& .MuiSwitch-track': {
+      opacity: 1,
+      backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      borderRadius: 20 / 2,
     },
-  },
-  '& .MuiSwitch-track': {
-    opacity: 1,
-    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-    borderRadius: 20 / 2,
-  },
-}));
+  }));
 
 
 
 
-function Darkmodetoggle(){
-  return <a href='/' className='modeswitch'>{<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}</a> 
-}
-//--------------------------Splash-----------------------
+  function Darkmodetoggle(){
+    return <a href='/' className='modeswitch'>{<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}</a> 
+  }
+  //--------------------------Splash-----------------------
 
-function Splash(){
-  return(
-    <div className='splashbk'>
-      <p>ok</p>
-    </div>
-  );
-}
+  // function Splash(){
+  //   return(
+  //     <div data-aos="fade-left" data-aos-duration="2000" className='splashbk'>
+  //       <BasicCard />
+  //     </div>
+  //   );
+  // }
 
-
-//---------------------------------------APP--------------------------------------------------
-
-
-function App() {
+  //-----------------------------------card------------------------------------
 
 
-
-
-const [title,settitles] = useState([]);
-const [load,loader] = useState(true);
-
-useEffect(function(){
-
-  axios.get('https://api.gyanibooks.com/library/get_dummy_notes/').then((res) =>{
-    settitles(res.data);
-    loader(false);
-  })
-},[]);
-
-
-
-
-
-
-
-
-
-  return (
-    
-    load ? <div> loading </div> :
-
-    <div className="body">
-      <Darkmodetoggle /> 
-      <div className="square">
-        <span></span>
-        <span></span>
-        <span></span>
+  function BasicCard({ chosenData }) {
+    var id=1; 
+    console.log(chosenData)
+    return (
+      <div data-aos="fade-left" data-aos-duration="2000" className='splashbk'>
+        <Card sx={{ minWidth: 300,width:300, height:200,position:'absolute',right:30,top:-70 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              id : {chosenData.id} | user : {chosenData.user}
+            </Typography>
+            <Typography variant="h5" component="div">
+              {chosenData.title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {chosenData.category}
+            </Typography>
+            <Typography variant="body2">
+              notes
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </div>
-      <p></p>
+    );
+  }
 
-      <div className="sidebar">
-        <PersistentDrawerLeft title={title} />
-      </div>
-      <p>{console.log(title)}</p>
-      <div className="bottom">
-        <div className="waves">
-          <div className="wave" id="wave1"></div>
-          <div className="wave" id="wave2"></div>
-          <div className="wave" id="wave3"></div>
-          <div className="wave" id="wave4"></div>
-        </div>
-      </div>
+  //---------------------------------------APP--------------------------------------------------
 
 
-      {/* <Paper className='content' sx={{
-        width:300,
-        height:300,
-        borderRadius:50,
-        backgroundColor: "#E3A857",
-        color: "red",
-      }} elevation={6}>
-        <Box p={1}>
-          <Typography variant="h5"></Typography>
-        </Box>
-      </Paper> */}
+  function App() {
 
-      <div className="content">
-        <p className="Greetings">Bonjour!</p>
-      </div>
 
-      <Splash />
 
+
+  const [title,settitles] = useState([]);
+  const [load,loader] = useState(true);
+  const [chosenData, setChosenData] = useState([]);
+
+  useEffect(function(){
+    Aos.init();
+    axios.get('https://api.gyanibooks.com/library/get_dummy_notes/').then((res) =>{
+      settitles(res.data);
+      loader(false);
+    })
+  },[]);
+
+
+
+
+
+
+
+
+
+    return (
       
-    </div>
-  );
-}
+      load ? <div> loading </div> :
 
-export default App;
+      <div className="body">
+        <Darkmodetoggle /> 
+        <div className="square">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <p></p>
+
+        <div className="sidebar">
+          <PersistentDrawerLeft title={title} setChosenData={setChosenData} />
+        </div>
+        <div className="bottom">
+          <div className="waves">
+            <div className="wave" id="wave1"></div>
+            <div className="wave" id="wave2"></div>
+            <div className="wave" id="wave3"></div>
+            <div className="wave" id="wave4"></div>
+          </div>
+        </div>
+
+
+
+
+        <div data-aos="zoom-out-up" data-aos-duration="1000" className="content">
+          <p className="Greetings">Bonjour!</p>
+        </div>
+
+        <BasicCard  chosenData={chosenData}/>
+
+        
+      </div>
+    );
+  }
+
+  export default App;
